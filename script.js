b56f4407b084ff28e4da085f01d744514a490a3f
omarovaassel <omarova.assel@gmail.com>
2022-10-18 21:16:21 +0600

    task with folders done

---
+++ script.js
@@ -0,0 +1,41 @@
+//18/10/22 показать родительскую папку subfolder
+
+let arr = [
+    {
+        name: "Folder 1",
+        folders: [
+            {name: "Subfolder 1"},
+            {name: "Subfolder 2"},
+            {name: "Subfolder 3"},
+        ]
+    },
+    {
+        name: "Folder 2",
+        folders: [
+            {name: "Subfolder 4"},
+            {name: "Subfolder 5"},
+            {name: "Subfolder 6"},
+        ]
+    },
+    {
+        name: "Folder 3",
+        folders: [
+            {name: "Subfolder 7"},
+            {name: "Subfolder 8"},
+            {name: "Subfolder 9"},
+        ]
+    },
+];
+
+function showParentFolderName(subFolderName){
+    for(let item of arr){
+        for(let i = 0; i < item.folders.length; i++){
+            if(subFolderName === item.folders[i].name){
+                console.log(item.name);
+                break; // останавливаем функцию, когда нашли subFolderName
+            }
+        }
+    }
+}
+
+showParentFolderName("Subfolder 7");
